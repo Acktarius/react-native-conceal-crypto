6a6cb5edea6a88ad4f28eba37f15ffca0de50cb9
acktarius <acktarius@protonmail.com>
2025-10-21 10:21:56 -0400

    lint fixes

--- scripts/fix-cmake-linking.js
+++ scripts/fix-cmake-linking.js
@@ -1,5 +1,4 @@
-import fs from 'fs';
-import path from 'path';
+import fs from 'node:fs';
 
 const AUTOLINKING_FILE = 'nitrogen/generated/android/ConcealCrypto+autolinking.cmake';
 
--- scripts/fix-prefab-bug.js
+++ scripts/fix-prefab-bug.js
@@ -1,5 +1,4 @@
-import fs from 'fs';
-import path from 'path';
+import fs from 'node:fs';
 
 const CMAKELISTS_FILE = 'android/CMakeLists.txt';
 
